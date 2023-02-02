a7b1bbd2013e9aa6a309388cb4b4f8b73600e4ed
SOMKUVAR <ayushisomkuvar2001.as@gamail.com>
2023-02-02 17:02:08 +0530

    created hover effect in mainPage

--- src/component/mainPage/MainPage.jsx
+++ src/component/mainPage/MainPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import CreateFlashCard from '../createFlashCard/CreateFlashCard'
-import { MyFlashCard } from '../myFlashCard/MyFlashCard'
+import { MyFlashCard } from '../myFlashCard/MyFlashCard';
+import './MainPage.css';
 
 export const MainPage = () => {
     return (
-        <div className="container-fluid my-12 mx-20">
+        <div className="container-fluid my-12 mx-24">
             <h2 className="text-xl text-black font-bold">Create Flash Card</h2>
-            <div>
-                
+            <div className="my-3">
+                <span className="line-hover">Create New </span>
+                <span className="line-hover">My FlashCard </span>
+                <hr/>
             </div>
             <CreateFlashCard/>
             <MyFlashCard/>
